53db49938e96ac3eb86e5b5561ae3b9035d6be46
tipptop3d <spamtop3d@gmail.com>
2022-12-03 18:59:51 +0100

    new path scaling that scalex in x-direction

--- src/components/PhysicPath.vue
+++ src/components/PhysicPath.vue
@@ -1,87 +1,64 @@
 <template>
-  <div class="container">
+  <div class="container" :style="{ width: `${width}px`, height: `${height}px` }">
     <svg ref="svgElement" preserveAspectRatio="xMinYMid meet"
-      :viewBox="`0 ${-viewBoxWidth / 2} ${viewBoxWidth} ${viewBoxHeight}`" display="block">
+      :viewBox="`0 ${-viewBoxHeight / 2} ${viewBoxWidth} ${viewBoxHeight}`" display="block">
       <path ref="pathElement" :d="path"></path>
     </svg>
     <div class="circle" :style="{ '--offset-path': `path('${scaledPath}')` }"></div>
   </div>
+  <div>
+    <input id="width" type="text" v-model="width" />
+    <label for="width">Width</label>
+    <input id="height" type="text" v-model="height" />
+    <label for="height">Height</label>
+    <input id="viewBoxWidth" type="text" v-model="viewBoxWidth" />
+    <label for="viewBoxWidth">ViewBoxWidth</label>
+    <input id="viewBoxHeight" type="text" v-model="viewBoxHeight" />
+    <label for="viewBoxHeight">ViewBoxHeight</label>
+    <input id="path" type="text" v-model="path" />
+    <label for="path">Path</label>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import * as d3 from 'd3'
+import { ref, onMounted, watch, computed } from 'vue'
 
-const viewBoxWidth = 16
-const viewBoxHeight = 16
+const width = ref(500);
+const height = ref(500);
+
+
+const viewBoxWidth = ref(16)
+const viewBoxHeight = ref(16)
 const path = ref('M 0 0 Q 7 5 8 0 Q 9 -4 16 0')
 
 const svgElement = ref<SVGElement | null>(null)
 const pathElement = ref<SVGPathElement | null>(null)
-const scaledPath = ref<string | null>()
-// const motionPath = new ResponsiveMotionPath({
-//   viewBoxHeight,
-//   viewBoxWidth,
-//   path
-// })
 
-type xy = [number, number]
-
-function convertPathToData(path: SVGPathElement) {
-  const data: xy[] = []
-
-  for (let p = 0; p < path.getTotalLength(); p++) {
-    const { x, y } = path.getPointAtLength(p);
-    data.push([x, y])
+const scaledPath = computed(() => {
+  if (svgElement.value == null) {
+    return null
   }
-  return data
-}
+  const factor = width.value / viewBoxWidth.value
 
-function getMaximums(data: xy[]): xy {
-  const x_points = data.map(point => point[0])
-  const y_points = data.map(point => point[1])
-  return [Math.max(...x_points), Math.max(...y_points)]
-}
-
-function getRatios(maxs: xy, width: number, height: number): xy {
-  return [maxs[0] / width, maxs[1] / height]
-}
-
-
-onMounted(() => {
-  const { width, height } = svgElement.value!.getBoundingClientRect()
-  const data = convertPathToData(pathElement.value!)
-  const maximums = getMaximums(data)
-  const ratios = getRatios(maximums, viewBoxWidth, viewBoxHeight)
-
-  const widthRatio = (viewBoxHeight - viewBoxWidth) / viewBoxHeight
-  const widthOffset = (ratios[0] * width) / 2
-
-  const heightRatio = (viewBoxWidth - viewBoxHeight) / viewBoxWidth
-  const heightOffset = (ratios[1] * height) / 2
-
-  const xScale = d3
-    .scaleLinear()
-    .domain([0, maximums[0]])
-    .range([widthOffset, width * widthRatio - widthOffset])
-  const yScale = d3
-    .scaleLinear()
-    .domain([0, maximums[1]])
-    .range([heightOffset, height * heightRatio - heightOffset])
-  const scaled_points: xy[] = data.map(point => [
-    xScale(point[0]), yScale(point[1])
-  ])
-  scaledPath.value = d3.line()(scaled_points)
+  return path.value.split(/[, ]/).filter(sym => sym !== '').map(sym => {
+    if (!Number.isNaN(+sym)) {
+      return +sym * factor
+    } else {
+      return sym
+    }
+  }).join(' ')
 })
+
 
 </script>
 
 <style scoped>
 .container {
-  height: 500px;
-  width: 500px;
   outline: 2px black solid;
   position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 
@@ -100,7 +77,8 @@ svg {
   height: 30px;
   background-color: aqua;
   position: absolute;
-  top: 100px;
+  top: calc(v-bind(height) / 2 * 1px);
+  left: 0;
   offset-path: var(--offset-path);
   animation: move 3000ms infinite alternate ease-in-out;
   ;

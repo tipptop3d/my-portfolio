85b7a6aea32ac585a56f654c2752a27176178d4f
tipptop3d <spamtop3d@gmail.com>
2022-12-05 22:02:02 +0100

    svg circle + path data, no calc necessary

--- src/components/PhysicPath.vue
+++ src/components/PhysicPath.vue
@@ -3,8 +3,8 @@
     <svg ref="svgElement" preserveAspectRatio="xMinYMid meet"
       :viewBox="`0 ${-viewBoxHeight / 2} ${viewBoxWidth} ${viewBoxHeight}`" display="block">
       <path ref="pathElement" :d="path"></path>
+      <circle r="0.5" :style="{ '--offset-path': `path('${path}')` }"></circle>
     </svg>
-    <div class="circle" :style="{ '--offset-path': `path('${scaledPath}')` }"></div>
   </div>
   <div>
     <input id="width" type="text" v-model="width" />
@@ -21,34 +21,34 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, watch, computed } from 'vue'
+import { ref, onMounted, computed } from 'vue'
+import { SVGPathData } from 'svg-pathdata'
 
-const width = ref(500);
-const height = ref(500);
-
+const width = ref(1920);
+const height = ref(1080);
 
 const viewBoxWidth = ref(16)
-const viewBoxHeight = ref(16)
-const path = ref('M 0 0 Q 7 5 8 0 Q 9 -4 16 0')
+const viewBoxHeight = ref(9)
 
-const svgElement = ref<SVGElement | null>(null)
 const pathElement = ref<SVGPathElement | null>(null)
 
-const scaledPath = computed(() => {
-  if (svgElement.value == null) {
-    return null
-  }
-  const factor = width.value / viewBoxWidth.value
+const path = ref('M 0 0 Q 7 5 8 0 Q 9 -4 16 0')
 
-  return path.value.split(/[, ]/).filter(sym => sym !== '').map(sym => {
-    if (!Number.isNaN(+sym)) {
-      return +sym * factor
-    } else {
-      return sym
-    }
-  }).join(' ')
+path.value = "M 0 0 L 16 4.5"
+
+const pathData = computed(() => new SVGPathData(path.value))
+const offset = ref(0)
+
+const gravity = 9.81 // m/s^2
+const mass = 1000 // g
+const slope = 29.5 // degree
+
+function acceleration() {
+  return mass * Math.tan(slope * Math.PI / 180)
+}
+
+onMounted(() => {
 })
-
 
 </script>
 
@@ -71,17 +71,16 @@ svg {
   outline: 2px red solid;
 }
 
-.circle {
-  border-radius: 50%;
-  width: 30px;
-  height: 30px;
-  background-color: aqua;
-  position: absolute;
-  top: calc(v-bind(height) / 2 * 1px);
-  left: 0;
+circle {
+  stroke: none;
+  fill: cyan;
   offset-path: var(--offset-path);
   animation: move 3000ms infinite alternate ease-in-out;
-  ;
+}
+
+input,
+label {
+  padding-right: 16px;
 }
 
 @keyframes move {
